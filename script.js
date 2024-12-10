1085b2de234588329ffc3717d49044953a8b70d1
Smilana498 <mm6085058@gmail.com>
2024-12-10 20:08:17 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// for (var i = 20; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// Светофор
+// var colors = prompt('Выберите цает');
+
+// switch (colors) {
+//     case "red":
+//         alert('stop');
+//         break;
+//     case "yellow":
+//         alert('Wait');
+//         break;
+//     case "green":
+//         alert("Gooo");
+//         break;
+//     default:
+//         alert("yellow")
+//    }
+
+//    console.log(colors);
+
+
+
